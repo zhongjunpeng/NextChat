248c00feabc12a81b05db13a697d676fedf614e4
zhongjp <zhongjunpeng@94ai.com>
2025-03-12 16:12:15 +0800

    add

--- app/constant.ts
+++ app/constant.ts
@@ -102,7 +102,7 @@ export const MAX_SIDEBAR_WIDTH = 500;
 export const MIN_SIDEBAR_WIDTH = 230;
 export const NARROW_SIDEBAR_WIDTH = 100;
 
-export const ACCESS_CODE_PREFIX = "nk-";
+export const ACCESS_CODE_PREFIX = "sk-";
 
 export const LAST_INPUT_KEY = "last-input";
 export const UNFINISHED_INPUT = (id: string) => "unfinished-input-" + id;
